38812b7d8c3ac85cd2dc1bab63a2fc34893a0c11
sasaki <skyo.97.admj@gmail.com>
2021-06-27 07:51:37 +0900

    完成しました！

--- api/Common/getTranslate.js
+++ api/Common/getTranslate.js
@@ -6,12 +6,14 @@ const translate = new AWS.Translate();
 
 exports.getTranslate = (input, inLang, outLang) => {
   return new Promise((resolve, reject) => {
+    // 必要なデータ
     const params = {
       Text: input,
       SourceLanguageCode: inLang,
       TargetLanguageCode: outLang,
     };
 
+    // 翻訳を行う
     translate.translateText(params, (err, data) => {
       if (err) {
         console.log(err);
